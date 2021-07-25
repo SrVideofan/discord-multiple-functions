c5f6648182affc269903d5b279f1ecd707e1643b
Miguel Angel <videofanxd10yt@gmail.com>
2021-07-25 17:00:48 -0500

    a

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const { curioso } = require('../utils/curioso')
 const { error } = require('../src/error')
-const { bola8, anser } = require('../utils/respuestas')
 const {  memevideo, memeimagen, momasos} = require('../utils/memes')
 
 module.exports = {
